0f2b6c580dcb9cb36853ead49814d68b282fc3f2
John Yoon <jyoon@Johns-MacBook-Pro.local>
2016-12-06 02:47:47 -0500

    Footer changes

--- src/assets/react/components/footer/footer.js
+++ src/assets/react/components/footer/footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Text from '../header/text';
 
 export default class Footer extends React.Component {
   render() {
     return (
       <div className='footer'>
+        <Text go={null} text="const JOHNYOON = new FullStackDeveloper();"/>
       </div>
     )
   }
