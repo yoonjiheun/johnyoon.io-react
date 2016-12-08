a0e64469afa09fc083cb821bd2057bc6212cc661
John Yoon <remnantjy@gmail.com>
2016-12-08 00:39:40 -0500

    Added mobile footer

--- src/assets/react/components/footer/footer.js
+++ src/assets/react/components/footer/footer.js
@@ -5,8 +5,13 @@ export default class Footer extends React.Component {
   render() {
     return (
       <div className='footer'>
+      <div className='default-footer'>
         <Text go={null} text="const JOHNYOON = new FullStackDeveloper();"/>
       </div>
+      <div className='mobile'>
+        <Text go={null} text="Contact me: me@johnyoon.io"/>
+      </div>
+    </div>
     )
   }
 }
